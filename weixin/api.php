561579adf2f1765c18ede44b2ef2ecda66e5f2a9
jx520 <jerryjin0630@qq.com>
2017-02-08 10:04:41 +0800

    公众号后台，先放上来

---
+++ weixin/api.php
@@ -0,0 +1,558 @@
+<?php
+	
+	//声明一个常量定义一个token值, token
+	define("TOKEN", "weixin");
+
+	//通过Wechat类， 创建一个对象
+	$wechatObj = new Wechat();
+	
+	//如果没有通过GET收到echostr字符串， 说明不是再使用token验证
+	if (!isset($_GET['echostr'])) {
+		//调用wecat对象中的方法响应用户消息
+   		$wechatObj->responseMsg();
+	}else{
+		//调用valid()方法，进行token验证
+	
+ 		$echoStr = $_GET["echostr"];
+
+      		  //valid signature , option
+       		 if($wechatObj->valid()){
+        		echo $echoStr;
+        		exit;
+        	}
+	}
+
+
+	//声明一个Wechat的类， 处理接收消息， 接收事件， 响应各种消息， 以及token验证
+	class Wechat {
+	     
+	//验证签名, 手册中原代码改写
+    	public function valid() {
+
+
+ 		 // you must define TOKEN by yourself
+       		 if (!defined("TOKEN")) {
+          		  throw new Exception('TOKEN is not defined!');
+      		  }
+        
+      		  $signature = $_GET["signature"];
+        	  $timestamp = $_GET["timestamp"];
+      		  $nonce = $_GET["nonce"];
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+       		 // use SORT_STRING rule
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+		
+   	 }
+
+    //响应消息处理
+    public function responseMsg()
+    {
+	//接收微新传过来的xml消息数据    
+	$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+	//如果接收到了就处理并回复
+	if (!empty($postStr)){
+	    //将接收到的XML字符串写入日志， 用R标记表示接收消息
+	    $this->logger("R \n".$postStr);
+	    //将接收的消息处理返回一个对象
+	    $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+	    //从消息对象中获取消息的类型 text  image location voice vodeo link 
+            $RX_TYPE = trim($postObj->MsgType);
+             
+            //消息类型分离, 通过RX_TYPE类型作为判断， 每个方法都需要将对象$postObj传入
+            switch ($RX_TYPE)
+	    {
+
+             	case "event":
+					$result = $this->receiveEvent($postObj);     //事件消息
+					break;
+                case "text":
+                    $result = $this->receiveText($postObj);     //接收文本消息
+                    break;
+                case "image":
+                    $result = $this->receiveImage($postObj);   //接收图片消息
+                    break;
+                case "location":
+                    $result = $this->receiveLocation($postObj);  //接收位置消息
+                    break;
+                case "voice":
+                    $result = $this->receiveVoice($postObj);   //接收语音消息 -----
+                    break;
+                case "video":
+                    $result = $this->receiveVideo($postObj);  //接收视频消息
+                    break;
+                case "link":
+                    $result = $this->receiveLink($postObj);  //接收链接消息
+                    break;
+                default:
+                    $result = "unknown msg type: ".$RX_TYPE;   //未知的消息类型
+                    break;
+	    }
+	    //将响应的消息再次写入日志， 使用T标记响应的消息！
+            $this->logger("T \n".$result);
+	    //输出消息给微信
+	    echo $result;
+	}else {
+	    //如果没有消息则输出空，并退出
+            echo "";
+            exit;
+        }
+    }
+
+     //接收事件消息
+    private function receiveEvent($object)
+    {
+		//包含函数库
+		include "func.inc.php";    
+
+		//临时定义一个变量， 不同的事件发生时， 给用户反馈不同的内容
+		$content = "";
+
+		//================================== 对用户触发的不同事件做处理 ===================================
+		switch ($object->Event)
+		{
+			//-------------------------- 用户关注 触发的事件 --------------------------
+			case "subscribe":
+
+                $content = array();
+                $content[] = array("Title"=>"欢迎光临！",  "Description"=>"大家好，我是笨笨，笨笨的笨，笨笨的笨，谢谢！", "PicUrl"=>"https://mmbiz.qlogo.cn/mmbiz_jpg/0sSkmBT5m0YSu61LribFl6Q7NfjficDnrclvJeVtMkSEYCY5jVEpnytZaGzmZkqTiaWa841SNnIYR18WdCEXf0kOw/0");
+				//通过事件中的xml转成的object对象中的FromUserName获取openid
+				$openid = $object->FromUserName;
+				//如果是扫描带参数的二维码
+				if(!empty($object->EventKey)) {
+					//将前缀去了， 只留下参数， 这个参数就是二维码中咱们指定的组ID
+					$groupid = str_replace("qrscene_","",$object->EventKey);
+					//调用func.inc.php中的adduser函数， 将用户加入到指定的组， 并加到指定的数据库
+					adduser( $openid, $groupid);
+
+					//如果用户传来EventKey事件， 则是扫描二维码的
+					$content .= "\n来自二维码场景 ".$groupid;
+
+				}else{
+					//如果是扫描自带的二维码使用下面函数添加
+					adduser($openid, 0);
+				}
+
+				break;
+			//-------------------------- 取消关注时触发的事件 --------------------------
+			case "unsubscribe":
+				$content = "取消关注";
+				//用户在取消关注时将wuser表中的关注列设置为0
+				$openid = $object->FromUserName;
+                //调用func.inc.php中的 deluser 函数， 将用户从数据库删除
+				deluser($openid);		
+
+				break;
+
+			//--------------------------  扫码事件 --------------------------
+			case "scancode_waitmsg":
+				$content = "scancode_waitmsg 扫码得到结果： ".$object->EventKey ."
+                ScanType： ".$object->ScanCodeInfo->ScanType ."
+                ScanResult： ".$object->ScanCodeInfo->ScanResult;
+                //------------- 扫图书 ISBN 码 -----------------
+                
+
+                //----------------------------------------------
+
+				break;
+
+            case "scancode_push":
+                $content = "scancode_push 扫码得到结果： ".$object->EventKey;
+
+                break;
+			//--------------------------  菜单点击事件 --------------------------
+			case "CLICK":
+				switch ($object->EventKey)
+				{
+					case "company":
+						$content = array();
+						$content[] = array("Title"=>"小规模低性能低流量网站设计原则",  "Description"=>"单图文内容", "PicUrl"=>"http://mmbiz.qpic.cn/mmbiz/2j8mJHm8CogqL5ZSDErOzeiaGyWIibNrwrVibuKUibkqMjicCmjTjNMYic8vwv3zMPNfichUwLQp35apGhiciatcv0j6xwA/0", "Url" =>"http://mp.weixin.qq.com/s?__biz=MjM5NDAxMDEyMg==&mid=201222165&idx=1&sn=68b6c2a79e1e33c5228fff3cb1761587#rd");
+						break;
+					case "Information":
+						$content = "姓名：金鑫
+生日：1984-12-31
+性别：男性
+民族：汉族
+年龄：33岁
+籍贯：湖南
+学历：大专";
+						break;
+					case "Profile":
+						$content = "本人的开发经验包括：Web前端、进销存系统、手机游戏。
+有 Unity 3D、Cocos2dx、Spine 项目经验，并熟练使用相关软件。 
+熟练掌握 Word、Excel、Visio、Axure、Photoshop 等常用办公和设计软件。
+具有强烈的进取心，有激情，能吃苦耐劳，能承受工作压力。
+责任心强，工作踏实认真，富有团队精神、爱好专研勇于挑战。";
+						break;
+					case "Experience":
+						$content = "【2015年1月 至 2016年04月	动网先锋网络科技有限公司	游戏策划】
+1.负责各类养成系统的设计和相关文档撰写，以及后期跟进。
+2.审核包括UI、音效、特效 在内的所有美术资源，协调开发进度。
+3.负责项目所需的辅助工具开发：导表工具、GM系统前端界面。
+【2013年8月 至 2014年12月	广州乐添信息科技有限公司	游戏策划】
+1.各类系统功能的设计并撰写相关文档。参与制定开发计划，并具体跟进执行。
+2.审核 UI、原画、动画等美术资源，协助美术优化特效和动作表现。
+3.协调程序美术工作，优化流程。为项目撰写Spine教程，设计相关解决方案。
+【2006年8月 至 2010年4月	深圳骏东科技有限公司	WEB开发】
+1.儿童游戏设计和开发。
+2.FLASH网站、动画设计和制作。
+3.红酒仓库进销存系统设计开发。";
+						break;
+					case "Contact":
+						$content = "有事请来电：
+18607437722";
+						break;
+					default:
+						$content = "不认识的菜单：".$object->EventKey;
+						break;
+				}
+
+				break;
+			//--------------------------  获取定位信息事件 --------------------------
+			case "LOCATION":
+				$content = "您当前的位置：纬度 ".$object->Latitude.";经度 ".$object->Longitude;
+				break;
+
+			//--------------------------  跳转到 URL 事件 --------------------------
+			case "VIEW":
+				$content = "跳转链接 ".$object->EventKey;
+				break;
+
+			//--------------------------  跳转到 URL 事件 --------------------------
+			case "MASSSENDJOBFINISH":
+				$content = "消息ID：".$object->MsgID."，结果：".$object->Status."，粉丝数：".$object->TotalCount."，过滤：".$object->FilterCount."，发送成功：".$object->SentCount."，发送失败：".$object->ErrorCount;
+				break;
+
+			//--------------------------  如果不属于以上任何事件那么 --------------------------
+			default:
+				$content = "receive a new event: ".$object->Event;
+				break;
+		}
+//================================== 对用户触发的不同事件做处理 END ===================================
+
+		//--------------------------  判断回复内容是否为数组，如果是继续处理 --------------------------
+		if(is_array($content)){
+			if (isset($content[0])){
+				$result = $this->transmitNews($object, $content);
+			}else if (isset($content['MusicUrl'])){
+				$result = $this->transmitMusic($object, $content);
+			}
+		}else{
+			$result = $this->transmitText($object, $content);
+		}
+
+		return $result;
+    }
+
+    //接收文本消息
+    private function receiveText($object)
+    {
+	//从接收到的消息中获取用户输入的文本内容， 作为一个查询的关键字， 使用trim()函数去两边的空格
+        $keyword = trim($object->Content);
+
+        //自动回复模式
+/*
+        if (strstr($keyword, "文本")){
+		     $content = "这是个文本消息";
+
+	    }else if (strstr($keyword, "单图文")){
+
+            $content = array();
+            $content[] = array("Title"=>"小规模低性能低流量网站设计原则",  "Description"=>"单图文内容", "PicUrl"=>"http://mmbiz.qpic.cn/mmbiz/2j8mJHm8CogqL5ZSDErOzeiaGyWIibNrwrVibuKUibkqMjicCmjTjNMYic8vwv3zMPNfichUwLQp35apGhiciatcv0j6xwA/0", "Url" =>"http://mp.weixin.qq.com/s?__biz=MjM5NDAxMDEyMg==&mid=201222165&idx=1&sn=68b6c2a79e1e33c5228fff3cb1761587#rd");
+
+        }else */
+        //自动回复模式
+        if (strstr($keyword, "简历") || strstr($keyword, "个人简历")){
+            $content = array();
+            $content[] = array("Title"=>"基本资料", "Description"=>"姓名：金鑫性别专湖南吉首", "PicUrl"=>"http://mmbiz.qpic.cn/mmbiz_jpg/0sSkmBT5m0YSu61LribFl6Q7NfjficDnrcTwGbLnPQwQqCrxtjC644zYjQma8Nib17Vy8z82JL6APUMzE7DUJbJnQ/0", "Url" =>"http://jerryjin0630.oschina.io/jerry/resume/index.pdf");
+
+            $content[] = array("Title"=>"个人简介", "Description"=>"本人自毕业后，一直在广州、深圳发展，现在决定回吉首工作，求职程序开发岗位。
+多年游戏策划兼职脚本开发，有Unity 3D、Cocos2dx、Spine 游戏项目经验。 
+编程技能：HTML、CSS、JavaScript、Jquery、VBA、SQL，因为工作的需要也接触过其它一些五花八门的语言和脚本。目前正在学习PHP。
+熟练掌握 Word、Excel、Visio、Axure、PS 等常用软件，熟悉程序美术之间的配合。
+责任心强，工作踏实认真，能吃苦耐劳，富有团队精神、爱好专研勇于挑战。", "PicUrl"=>"https://mmbiz.qlogo.cn/mmbiz_gif/0sSkmBT5m0YSu61LribFl6Q7NfjficDnrc8NmVB4CvHVU5UgRkovLHHZiaLOSxnAia5JzJicGUhYPr1opV7ds1CKocA/s640", "Url" =>"http://jerryjin0630.oschina.io/jerry/resume/index.pdf");
+
+            $content[] = array("Title"=>"工作经验", "Description"=>"2015年1月 至 2016年04月 
+动网先锋网络科技有限公司
+游戏策划
+1. 负责各类养成系统的设计和相关文档撰写，以及后期跟进。
+2. 审核包括UI、音效、特效 在内的所有美术资源，协调开发进度。
+3. 负责项目所需的辅助工具开发：导表工具、GM系统前端界面。
+2013年8月 至 2014年12月
+广州乐添信息科技有限公司
+游戏策划
+1. 各类系统功能的设计并撰写相关文档。参与制定开发计划，并具体跟进执行。
+2. 审核 UI、原画、动画等美术资源，协助美术优化特效和动作表现。
+3. 协调程序美术工作，优化流程。为项目撰写Spine教程，设计相关解决方案。
+2006年8月 至 2010年4月
+深圳骏东科技有限公司
+游戏策划
+1. 儿童游戏设计和开发。
+2. FLASH网站、动画设计和制作。
+3. 红酒仓库进销存系统设计开发。
+", "PicUrl"=>"https://mmbiz.qlogo.cn/mmbiz_jpg/0sSkmBT5m0YSu61LribFl6Q7NfjficDnrcvURL5Z5NpXAe6UaQ2NhXZrs93ibBbicORicGU7dEpYUiaIUSCO52ibZKCEw/0?wx_fmt=jpeg", "Url" =>"http://jerryjin0630.oschina.io/jerry/resume/index.pdf");
+
+            $content[] = array("Title"=>"联系方式", "Description"=>"手机：18607437722
+微信：Jerryjin_  （可以直接扫码添加）
+邮箱：jerryjin0630@QQ.com", "PicUrl"=>"https://mmbiz.qlogo.cn/mmbiz_png/0sSkmBT5m0YSu61LribFl6Q7NfjficDnrcZDhia8Fz5JT5r1EVNrHrwvbR5HoTnVoOPYq7qnGgpHmQiaFzckTgIdqQ/0", "Url" =>"http://jerryjin0630.oschina.io/jerry/resume/index.pdf");
+
+
+        }else if (strstr($keyword, "音乐")){
+            $content = array();
+            $content = array("Title"=>"come some music", "Description"=>"来首歌听听", "MusicUrl"=>"http://jerryjin.5gbfree.com/music/wwd.mp3", "HQMusicUrl"=>"http://jerryjin.5gbfree.com/music/wwd.mp3");
+        }else{//如果都没有就。。。
+            
+            //$content = date("Y-m-d H:i:s",time())."\n你说啥，笨笨听不懂！哈哈哈哈";
+            $content = "你说啥，笨笨听不懂！哈哈哈哈";
+        }
+
+
+        //处理回复消息内容
+        if(is_array($content)){
+            if (isset($content[0]['PicUrl'])){
+                $result = $this->transmitNews($object, $content);
+            }else if (isset($content['MusicUrl'])){
+                $result = $this->transmitMusic($object, $content);
+            }
+        }else{
+            $result = $this->transmitText($object, $content);
+        }
+     
+
+        return $result;
+    }
+
+    //接收图片消息
+    private function receiveImage($object)
+    {
+        $content = array("MediaId"=>$object->MediaId);
+        $result = $this->transmitImage($object, $content);
+        return $result;
+    }
+
+    //接收位置消息
+    private function receiveLocation($object)
+    {
+        $content = "你发送的是位置，纬度为：".$object->Location_X."；经度为：".$object->Location_Y."；缩放级别为：".$object->Scale."；位置为：".$object->Label;
+        $result = $this->transmitText($object, $content);
+        return $result;
+    }
+
+    //接收语音消息
+    private function receiveVoice($object)
+    {
+	
+		/*
+		
+			//如果开启语言识别功能， 就可以使用这个
+        if (isset($object->Recognition) && !empty($object->Recognition)){
+            $content = "你刚才说的是：".$object->Recognition;
+            $result = $this->transmitText($object, $content);
+        }else{
+			$content = "未开启语音识别功能或者识别内容为空";
+			 $result = $this->transmitText($object, $content);
+		}
+		
+		
+		*/
+	
+		//如果开启语言识别功能， 就可以使用这个
+        if (isset($object->Recognition) && !empty($object->Recognition)){
+            $content = "你刚才说的是：".$object->Recognition;
+            $result = $this->transmitText($object, $content);
+        }else{
+            $content = array("MediaId"=>$object->MediaId);
+            $result = $this->transmitVoice($object, $content);
+        }
+
+        return $result;
+    }
+
+    //接收视频消息
+    private function receiveVideo($object)
+    {
+        //$content = array("MediaId"=>$object->MediaId, "Title"=>"视频调试，原样返回!", "Description"=>"pai pai");
+        //$result = $this->transmitVideo($object, $content);
+        $content = "视频的 MediaId： ".$object->MediaId;
+        $result = $this->transmitText($object, $content);
+        return $result;
+    }
+
+    //接收链接消息
+    private function receiveLink($object)
+    {
+        $content = "你发送的是链接，标题为：".$object->Title."；内容为：".$object->Description."；链接地址为：".$object->Url;
+        $result = $this->transmitText($object, $content);
+        return $result;
+    }
+//======================================  回复消息  ===============================================
+    //回复文本消息
+    private function transmitText($object, $content)
+    {
+        $xmlTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[%s]]></Content>
+</xml>";
+        $result = sprintf($xmlTpl, $object->FromUserName, $object->ToUserName, time(), $content);
+        return $result;
+    }
+
+    //回复图片消息
+    private function transmitImage($object, $imageArray)
+    {
+        $itemTpl = "<Image>
+    <MediaId><![CDATA[%s]]></MediaId>
+</Image>";
+
+        $item_str = sprintf($itemTpl, $imageArray['MediaId']);
+
+        $xmlTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[image]]></MsgType>
+$item_str
+</xml>";
+
+        $result = sprintf($xmlTpl, $object->FromUserName, $object->ToUserName, time());
+        return $result;
+    }
+
+    //回复语音消息
+    private function transmitVoice($object, $voiceArray)
+    {
+        $itemTpl = "<Voice>
+    <MediaId><![CDATA[%s]]></MediaId>
+</Voice>";
+
+        $item_str = sprintf($itemTpl, $voiceArray['MediaId']);
+
+        $xmlTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[voice]]></MsgType>
+$item_str
+</xml>";
+
+        $result = sprintf($xmlTpl, $object->FromUserName, $object->ToUserName, time());
+        return $result;
+    }
+
+    //回复视频消息
+    private function transmitVideo($object, $videoArray)
+    {
+        $itemTpl = "<Video>
+    <MediaId><![CDATA[%s]]></MediaId>
+    <Title><![CDATA[%s]]></Title>
+    <Description><![CDATA[%s]]></Description>
+</Video>";
+
+        $item_str = sprintf($itemTpl, $videoArray['MediaId'], $videoArray['Title'], $videoArray['Description']);
+
+        $xmlTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[video]]></MsgType>
+$item_str
+</xml>";
+
+        $result = sprintf($xmlTpl, $object->FromUserName, $object->ToUserName, time());
+        return $result;
+    }
+
+    //回复图文消息
+    private function transmitNews($object, $newsArray)
+    {
+        if(!is_array($newsArray)){
+            return;
+        }
+        $itemTpl = "    <item>
+        <Title><![CDATA[%s]]></Title>
+        <Description><![CDATA[%s]]></Description>
+        <PicUrl><![CDATA[%s]]></PicUrl>
+        <Url><![CDATA[%s]]></Url>
+    </item>
+";
+        $item_str = "";
+        foreach ($newsArray as $item){
+            $item_str .= sprintf($itemTpl, $item['Title'], $item['Description'], $item['PicUrl'], $item['Url']);
+        }
+        $xmlTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[news]]></MsgType>
+<ArticleCount>%s</ArticleCount>
+<Articles>
+$item_str</Articles>
+</xml>";
+
+        $result = sprintf($xmlTpl, $object->FromUserName, $object->ToUserName, time(), count($newsArray));
+        return $result;
+    }
+
+    //回复音乐消息
+    private function transmitMusic($object, $musicArray)
+    {
+        $itemTpl = "<Music>
+    <Title><![CDATA[%s]]></Title>
+    <Description><![CDATA[%s]]></Description>
+    <MusicUrl><![CDATA[%s]]></MusicUrl>
+    <HQMusicUrl><![CDATA[%s]]></HQMusicUrl>
+</Music>";
+
+        $item_str = sprintf($itemTpl, $musicArray['Title'], $musicArray['Description'], $musicArray['MusicUrl'], $musicArray['HQMusicUrl']);
+
+        $xmlTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[music]]></MsgType>
+$item_str
+</xml>";
+
+        $result = sprintf($xmlTpl, $object->FromUserName, $object->ToUserName, time());
+        return $result;
+    }
+
+
+
+    //日志记录
+    private function logger($log_content)
+    {
+      
+	    $max_size = 100000;   //声明日志的最大尺寸
+
+	    $log_filename = "log.xml";  //日志名称
+
+	    //如果文件存在并且大于了规定的最大尺寸就删除了
+	    if(file_exists($log_filename) && (abs(filesize($log_filename)) > $max_size)){
+		    unlink($log_filename);
+	    }
+
+	    //写入日志，内容前加上时间， 后面加上换行， 以追加的方式写入
+	    file_put_contents($log_filename, date('H:i:s')." ".$log_content."\n", FILE_APPEND);
+        
+    }
+}
+
+
+
+?>
+
