0934902fbdbef02bd089b16ff6c45bd715e8e99d
jx520 <jerryjin0630@qq.com>
2017-02-08 12:34:08 +0800

    添加 查看 log.xml 的链接
    Signed-off-by: jx520 <jerryjin0630@qq.com>

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-<<<<<<< HEAD
-	echo "大王叫我，来...巡..山....罗哦...";
-	echo "<a href = 'phpinfo.php'>查看 PHPINFO</a>";
-	echo "<a href = 'weixin/log.xml'>查看 log</a>";
-=======
 	echo "大王叫我，来...巡..山....罗哦...遇了南山，遇北山罗哦...";
-	echo "<a href = 'phpinfo.php'>phpinfo</a>"
->>>>>>> 4437d60bbbc478cbeba764fb3587df8f8641d4b8
+	echo "<a href = 'phpinfo.php'>phpinfo</a>";
+	echo "<a href = 'weixin/log.xml'>查看 log</a>";
 ?>
